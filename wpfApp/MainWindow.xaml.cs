b8a38fa43b31b2118a7f2a755cbf16932c020414
Toha11rs <son07027q@gmail.com>
2024-06-25 02:15:52 +0300

    1

---
+++ wpfApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DauevaApp.Model;
+using DauevaApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DauevaApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NavigateClass.frmNav = FrmMain;
+            FrmMain.Navigate(new Auth());
+
+
+
+            ConnectionDb.autoTransEntities = new AutoTransEntities();
+        }
+    }
+}
